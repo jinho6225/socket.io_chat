0824a85fd462c0e778d5cc70724c74ee2bff1438
jinho6225 <jinho6225@hotmail.com>
2020-08-21 17:14:26 -0700

    process socket_chat - connection completed client-server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const socket = require('socket.io')
+const path = require('path')
+
+const PORT = 3000
+const app = express();
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const io = socket(server);
+const activeUsers = new Set();
+
+io.on("connection", (socket) => {
+  console.log("made socket connection")
+
+  socket.on("new user", (data) => {
+    socket.userId = data;
+    activeUsers.add(data)
+    io.emit("new user", [...activeUsers])
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+  socket.on("chat message", (data) => {
+    io.emit("chat message", data)
+  })
+})
